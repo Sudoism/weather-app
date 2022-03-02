e8c31191b78c455cc2a5cc42e708d73fdcc62d9c
Alexander Hed <alexander.hed@gmail.com>
2022-03-02 18:14:23 +0100

    function sceleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./style.css";
+
+//hits the open weather API async/await style
+
+async function getWeather(location){
+    try {
+
+    }
+    catch {
+        
+    }
+}
+//process data from open weather, cleaning it up to be used in app
